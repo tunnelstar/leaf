e5845a6ee924ab9f017370ce05d74418cc4d7bb4
Shaw <cuzval@gmail.com>
2024-08-05 14:42:57 +0800

    Amend

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-# 指定你想要搜索的目录，你可以替换为你自己的目录
-directory=".."
-# 改名后的
-target="moss"
+# 获取脚本的绝对路径
+script_path=$(realpath "$0")
+
+# 设置 directory 为脚本所在目录的上级目录
+directory=$(dirname "$(dirname "$script_path")")
+
 # 获取脚本文件的名称
-script_name=$(basename "$0")
+script_name=$(basename "$script_path")
+
+# 改名后的目标
+target="moss"
+
+echo "The working directory (parent of the script directory) is: $directory"
+
+# 切换到工作目录
+cd "$directory" || { echo "Failed to change to directory $directory"; exit 1; }
 
 # 询问用户确认
 echo "This script will replace 'leaf' with '$target' in all file names and file contents under $directory"
-echo "Excluding this script file ($script_name)"
+echo "Excluding this script file ($script_name) and .git directory"
 read -p "Do you want to continue? (y/n): " confirm
 
 if [[ $confirm != [Yy]* ]]; then
@@ -28,7 +38,7 @@ replace_content() {
 
 # 使用 find 命令查找目录中的所有文件名包含 "leaf" 的文件
 # 使用 mv 命令将文件名中的 "leaf" 替换为 $target
-find "$directory" -depth -name "*leaf*" -not -name "$script_name" -print0 | while IFS= read -r -d '' file; do
+find . -path "./.git" -prune -o -depth -name "*leaf*" -not -name "$script_name" -print0 | while IFS= read -r -d '' file; do
     newfile=$(echo "$file" | sed "s/leaf/$target/g")
     if [ "$file" != "$newfile" ]; then
         if mv "$file" "$newfile"; then
@@ -43,7 +53,7 @@ find "$directory" -depth -name "*leaf*" -not -name "$script_name" -print0 | whil
 done
 
 # 处理不包含 "leaf" 在文件名中的文件
-find "$directory" -type f -not -name "*leaf*" -not -name "$script_name" -print0 | while IFS= read -r -d '' file; do
+find . -path "./.git" -prune -o -type f -not -name "*leaf*" -not -name "$script_name" -print0 | while IFS= read -r -d '' file; do
     replace_content "$file"
 done
 
